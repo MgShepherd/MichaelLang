4c0d14284ae786c5f2c7d527e938e6682a2d6a09
MgShepherd <michaelshep1504@gmail.com>
2026-08-11 13:19:24 +0100

    Started adding llvm logic to build return statements

--- include/llvm_ir.h
+++ include/llvm_ir.h
@@ -3,6 +3,12 @@
 
 #include "parser.h"
 
+/*
+ * llvm_ir_from_program takes a built program ASt and will generate the LLVM code for this
+ * Currently just outputs the generate code to stdout, but will eventually be to an obj file
+ * All LLVM state is cleaned up as part of this function, no additional cleanup neccessary
+ * Returns 0 on success, 1 on error
+ */
 unsigned char llvm_ir_from_program(const Program *program);
 
 #endif // _LLVM_IR_H_
